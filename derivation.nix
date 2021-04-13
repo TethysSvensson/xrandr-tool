046a9e66d5dcad40053ec64be6d95c34770a59e4
Tethys Svensson <freaken@freaken.dk>
2021-04-13 17:32:41 +0200

    Bump lockfiles and cargo hash

--- derivation.nix
+++ derivation.nix
@@ -6,5 +6,5 @@ rustPlatform.buildRustPackage rec {
   pname = "xrandr-tool";
   version = "0.1.0";
   src = ./.;
-  cargoSha256 = "0m93zybw31zh2qg0jyca9k1jch88vacyv93jglbbqjr3dying24q";
+  cargoSha256 = "12d2n0k10yy6dgi5i55ab41zva3ccpzzkj0qc4x43kqpxsb02piv";
 }
--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/20.09";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { nixpkgs, self, ... }@inputs:
