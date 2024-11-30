5baa506f53bd0022a65e7343c00d70d8cd7ecfcd
Tethys Svensson <freaken@freaken.dk>
2024-11-30 20:54:43 +0100

    Update stuff

--- derivation.nix
+++ derivation.nix
@@ -6,5 +6,5 @@ rustPlatform.buildRustPackage rec {
   pname = "xrandr-tool";
   version = "0.1.0";
   src = ./.;
-  cargoSha256 = "12d2n0k10yy6dgi5i55ab41zva3ccpzzkj0qc4x43kqpxsb02piv";
+  cargoHash = "sha256-5KVlZlcgP1B2SmfrVCIQXOHw/MEKuFW8JrAEwEEYuWI=";
 }
