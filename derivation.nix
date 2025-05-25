cdd41bf254ad450cee296b99f5496210c0c9379e
Tethys Svensson <freaken@freaken.dk>
2025-05-25 19:53:23 +0200

    Fix cargo hash

--- derivation.nix
+++ derivation.nix
@@ -6,5 +6,5 @@ rustPlatform.buildRustPackage rec {
   pname = "xrandr-tool";
   version = "0.1.0";
   src = ./.;
-  cargoHash = "sha256-IPIKCwzzfVNPVEVhssyaqgpvaldPNTBR0/4sQOA19sA=";
+  cargoHash = "sha256-mpslJm/aB59KcTKcn8sowxBX59SympJkTY0KHk0Pynk=";
 }
