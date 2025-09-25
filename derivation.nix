429ce23ea6fad9d55c58c042710b6497726f1276
Tethys Svensson <freaken@freaken.dk>
2025-09-25 13:42:58 +0200

    Bump dependnecies

--- derivation.nix
+++ derivation.nix
@@ -6,5 +6,5 @@ rustPlatform.buildRustPackage rec {
   pname = "xrandr-tool";
   version = "0.1.0";
   src = ./.;
-  cargoHash = "sha256-mpslJm/aB59KcTKcn8sowxBX59SympJkTY0KHk0Pynk=";
+  cargoHash = "sha256-3cRXduBaAx+BSR+JmPKCeUnIWG9as5bTcqeX5nYpvZ0=";
 }
